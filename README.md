fae1069172d17d4480bb4410d2e8e24c392fbe94
M. O. Marmalade <xephyrpanda@yahoo.com>
2021-01-12 20:08:34 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,56 @@
+![Flourish-small](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/flourish-small.png)
+
+A custom tool for Renoise, used to create flourishes, strums, ripples, arpeggios, etc
+
+[OFFICIAL PAGE](https://xephyrpanda.wixsite.com/citrus64/flourish)
+
+![FlourishExample](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/flourishsample.PNG)
+
+## Controls
+![Flourish Window](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/flourishwindow.PNG)
++ Move the Time![clock](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/clock.bmp) slider to spread the notes over time (the range of this slider can be extended by the rotary knob just below it)
++ Move the Tension![curve](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/curve.bmp) slider to change the distribution of the notes
++ Move the Offset![Arrows](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/arrows.bmp) slider to shift all notes over time (the range of this slider can be extended by the rotary knob just below it)
++ Click the Non-Destructive![stilts](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/stilts.bmp)/![steamroller](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/steamroller.bmp) button to toggle Non-Destructive Mode on/off
++ Use the "Set Line" button (or the keyboard shortcut/menu entries) to set a new line to be edited by Flourish
++ Click the Help![Question Mark](https://raw.githubusercontent.com/M-O-Marmalade/com.MOMarmalade.Flourish.xrnx/master/Bitmaps/question.bmp) button to view this page at any time
+
+**Note:** The more lines/patterns your currently edited flourish spans over, the more the tool will lag.
+
+## Activating Flourish on a Line
++ Flourish uses the pattern edit cursor's position to select a line for processing (it does not support box selection)\
+![ExampleGIF1](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/flourishexample1.gif)
+
++ You can use the **right-click menu** or **user-assignable keyboard shortcut** to activate Flourish on the currently edited line\
+![Right Click Menu](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/rightclick.jpg)\
+![Keyboard Shortcut](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/keyboardshortcut.jpg)
+
+## Keyboard Shortcuts
+While the Flourish window is in focus, there are some keyboard shortcuts supported by the tool
++ Hold the **[SPACEBAR]** to preview the currently processed flourish/chord
++ Use the **[ARROW KEYS ![Up/Down Arrows](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/updownArrows.png)![Left/Right Arrows](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/leftrightArrows.png)]** to navigate the edit cursor
++ Press **[CTRL + SHIFT + X]** to select a new line to be processed
++ Press **[CTRL + ![Up/Down Arrows](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/updownArrows.png)]** to move the Time slider
++ Press **[CTRL + ![Left/Right Arrows](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/leftrightArrows.png)]** to increase/decrease the Time multiplier knob
+
+# HOW TO INSTALL
+
+***Just download the [latest version](https://github.com/M-O-Marmalade/mom.MOMarmalade.Flourish.xrnx/releases/latest/) from the Releases section above!*** If you would like the latest experimental build though, you can download it by following the steps/GIFs below!
+
+1. **Download** the tool from this repository\
+![Download](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/downloadflourish.gif)
+
+
+2. **Extract** the *"com.MOMarmalade.Flourish.xrnx-master"* folder from the .zip file\
+![Extract](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/extractflourish.gif)
+
+
+3. **Rename** the extracted folder to remove the *"-master"* from the end of the name\
+![Rename](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/renameflourish.gif)
+
+
+4. **Drag and Drop** the renamed folder onto an open Renoise window to install the tool\
+![Drag n' Drop](https://raw.githubusercontent.com/M-O-Marmalade/Pix/master/dragndropflourish.gif)
+
+
+...and it's ready to use! (The installation files can now be deleted)
---
+++ main.lua
@@ -0,0 +1,357 @@
+--Strum - main.lua--
+--DEBUG CONTROLS-------------------------------
+local debugmode = true 
+
+if debugmode then
+  _AUTO_RELOAD_DEBUG = true
+end
+
+local debugvars = {
+  create_note_offs_on_find = false,
+  create_note_offs_on_calculate_single_note = false,
+  print_note_placements = false,
+  print_new_note_placements = false,
+  clear_pattern_one = true
+}
+
+--GLOBALS-------------------------------------------------------------------------------------------- 
+local app = renoise.app() 
+local tool = renoise.tool()
+local song = nil
+
+local vb = renoise.ViewBuilder() 
+local window_obj = nil
+local window_title = "Strum" 
+local window_content = nil 
+local default_margin = 0
+local sliders_width = 22
+local sliders_height = 127
+local multipliers_size = 23
+
+local selected_seq
+local selected_pattern
+local selection
+
+local visible_note_columns = {}
+local column_to_end_on_in_first_track
+
+local notes_in_selection = {}
+local total_delay_range
+
+
+
+local time
+local time_max = 4
+
+local time_multiplier = 1
+local time_multiplier_max = 64
+
+--CLEAR VARIABLES------------------------------
+local function clear_variables()
+
+  visible_note_columns = {}
+  notes_in_selection = {}
+  
+  return(true)
+end
+
+--CREATE NOTE-OFF--------------------------------
+local function create_note_off(p,t,c,l)
+
+  song:pattern(p):track(t):line(l):note_column(c).note_value = 120
+
+end
+
+--STORE NOTE--------------------------------------------
+local function store_note(p,t,c,l)
+  
+  local column = song:pattern(p):track(t):line(l):note_column(c)
+  
+  if not column.is_empty then
+  
+    if not notes_in_selection[p] then notes_in_selection[p] = {} end
+    if not notes_in_selection[p][t] then notes_in_selection[p][t] = {} end
+    if not notes_in_selection[p][t][c] then notes_in_selection[p][t][c] = {} end
+    if not notes_in_selection[p][t][c][l] then notes_in_selection[p][t][c][l] = {} end
+  
+    notes_in_selection[p][t][c][l][1] = column.note_value
+    notes_in_selection[p][t][c][l][2] = column.instrument_value
+    notes_in_selection[p][t][c][l][3] = column.volume_value 
+    notes_in_selection[p][t][c][l][4] = column.panning_value 
+    notes_in_selection[p][t][c][l][5] = column.delay_value 
+    notes_in_selection[p][t][c][l][6] = column.effect_number_value 
+    notes_in_selection[p][t][c][l][7] = column.effect_amount_value 
+  
+  end
+  
+  return(true)
+end
+
+--FIND NOTES IN SELECTION---------------------------------------------
+local function find_notes_in_selection()
+  
+  song = renoise.song()
+  
+  --get selection
+  selected_seq = song.selected_sequence_index
+  selected_pattern = song.sequencer:pattern(selected_seq)
+  selection = song.selection_in_pattern
+  
+  if not selection then
+    app:show_error("no selection has been made")
+    return(false)
+  end
+  
+  --determine which columns are visible
+  for t = selection.start_track, selection.end_track do  
+    visible_note_columns[t] = song:track(t).visible_note_columns     
+  end  
+    
+  --find out what column to end on when working in the first track, based on how many tracks are selected total
+  if selection.end_track - selection.start_track == 0 then
+    column_to_end_on_in_first_track = math.min(selection.end_column, visible_note_columns[selection.start_track])
+  else
+    column_to_end_on_in_first_track = visible_note_columns[selection.start_track]
+  end
+  
+  --work on first track--
+  for c = selection.start_column, column_to_end_on_in_first_track do
+    for l = selection.start_line, selection.end_line do
+      store_note(selected_pattern,selection.start_track,c,l) 
+      
+      if debugvars.create_note_offs_on_find then
+        create_note_off(selected_pattern,selection.start_track,c,l)
+      end           
+    end
+  end
+  
+  --work on middle tracks--
+  if selection.end_track - selection.start_track > 1 then
+    for t = selection.start_track + 1, selection.end_track - 1 do
+      for c = 1, visible_note_columns[t] do
+        for l = selection.start_line, selection.end_line do
+          store_note(selected_pattern,t,c,l)   
+          
+          if debugvars.create_note_offs_on_find then
+            create_note_off(selected_pattern,t,c,l)
+          end          
+        end      
+      end    
+    end
+  end
+  
+  --work on last track--
+  if selection.end_track - selection.start_track > 0 then
+    for c = 1, math.min(selection.end_column, visible_note_columns[selection.end_track]) do
+      for l = selection.start_line, selection.end_line do
+        store_note(selected_pattern,selection.end_track,c,l) 
+        
+        if debugvars.create_note_offs_on_find then
+          create_note_off(selected_pattern,selection.end_track,c,l)
+        end        
+      end
+    end
+  end
+  
+  return(true)
+end
+
+--CALCULATE RANGE------------------------------------------
+local function calculate_range()
+
+  total_delay_range = (selection.end_line - (selection.start_line - 1))*256
+
+  return(true)
+end
+
+--CALCULATE SINGLE NOTE PLACEMENT------------------------------------------
+local function calculate_single_note_placement(p,t,c,l)
+
+  if notes_in_selection[p][t][c][l] then 
+  
+    if debugvars.create_note_offs_on_calculate_single_note then
+      create_note_off(p,t,c,l)
+    end
+    
+    local line_difference = l - selection.start_line
+    
+    local delay_difference = notes_in_selection[p][t][c][l][5] + (line_difference*256)
+    
+    local note_place = delay_difference / total_delay_range
+    
+    notes_in_selection[p][t][c][l][8] = note_place
+    
+    if debugvars.print_note_placements then
+      print(("pattern %i, track %i, column %i, line %i, placement: %f"):format(p,t,c,l,note_place))
+    end    
+  end
+  
+  return(true)
+end
+
+--CALCULATE NOTE PLACEMENTS------------------------------------------
+local function calculate_note_placements()
+
+  --work on first track--
+  for c = selection.start_column, column_to_end_on_in_first_track do
+    for l = selection.start_line, selection.end_line do
+      calculate_single_note_placement(selected_pattern,selection.start_track,c,l)
+    end
+  end
+  
+  --work on middle tracks--
+  if selection.end_track - selection.start_track > 1 then
+    for t = selection.start_track + 1, selection.end_track - 1 do
+      for c = 1, visible_note_columns[t] do
+        for l = selection.start_line, selection.end_line do
+          calculate_single_note_placement(selected_pattern,t,c,l)     
+        end      
+      end    
+    end
+  end
+  
+  --work on last track--
+  if selection.end_track - selection.start_track > 0 then
+    for c = 1, math.min(selection.end_column, visible_note_columns[selection.end_track]) do
+      for l = selection.start_line, selection.end_line do
+        calculate_single_note_placement(selected_pattern,selection.end_track,c,l)   
+      end
+    end
+  end
+
+  return(true)
+end
+
+--PLACE NEW NOTE----------------------------------------------
+local function place_new_note(p,t,c,l)
+  
+  if notes_in_selection[p][t][c][l] then
+  
+    local new_placement = notes_in_selection[p][t][c][l][8] * (time * time_multiplier + 1)
+    
+    local new_delay_difference = new_placement*total_delay_range
+    
+    local new_line_difference = math.floor(new_delay_difference / 256)
+    
+    local new_delay_value = new_delay_difference%256
+    
+    local new_line = selection.start_line + new_line_difference
+  
+    local column = song:pattern(p):track(t):line(new_line):note_column(c)
+  
+    column.note_value = notes_in_selection[p][t][c][l][1]
+    column.instrument_value = notes_in_selection[p][t][c][l][2]
+    column.volume_value = notes_in_selection[p][t][c][l][3]
+    column.panning_value = notes_in_selection[p][t][c][l][4]
+    column.delay_value = new_delay_value
+    column.effect_number_value = notes_in_selection[p][t][c][l][6]
+    column.effect_amount_value = notes_in_selection[p][t][c][l][7]
+    
+    if debugvars.print_new_note_placements then
+      print(("pattern %i, track %i, column %i, line %i, new placement: %f"):format(p,t,c,l, new_placement))
+    end
+  
+  end
+
+  return(true)
+end
+
+--STRUM SELECTION------------------------------------------
+local function strum_selection()
+
+  if debugvars.clear_pattern_one then
+    song:pattern(1):clear()
+  end
+
+  --work on first track--
+  for c = selection.start_column, column_to_end_on_in_first_track do
+    for l = selection.start_line, selection.end_line do
+      place_new_note(selected_pattern,selection.start_track,c,l)
+    end
+  end
+  
+  --work on middle tracks--
+  if selection.end_track - selection.start_track > 1 then
+    for t = selection.start_track + 1, selection.end_track - 1 do
+      for c = 1, visible_note_columns[t] do
+        for l = selection.start_line, selection.end_line do
+          place_new_note(selected_pattern,t,c,l)     
+        end      
+      end    
+    end
+  end
+  
+  --work on last track--
+  if selection.end_track - selection.start_track > 0 then
+    for c = 1, math.min(selection.end_column, visible_note_columns[selection.end_track]) do
+      for l = selection.start_line, selection.end_line do
+        place_new_note(selected_pattern,selection.end_track,c,l)   
+      end
+    end
+  end
+
+  return(true)
+end
+
+--SHOW WINDOW---------------------------------------------------- 
+local function show_window()
+
+  --prepare the window content if it hasn't been done yet
+  if not window_content then    
+    window_content = vb:column {
+  
+      vb:minislider {    
+      id = "time_slider", 
+      tooltip = "The time over which to spread the notes", 
+      min = -time_max, 
+      max = time_max, 
+      value = 0, 
+      width = sliders_width, 
+      height = sliders_height, 
+      notifier = function(value)
+        time = value
+        strum_selection()
+      end    
+      },
+      
+      vb:rotary { 
+        id = "time_multiplier", 
+        tooltip = "Time multiplier", 
+        min = 1, 
+        max = time_multiplier_max, 
+        value = 1, 
+        width = multipliers_size, 
+        height = multipliers_size, 
+        notifier = function(value) 
+          time_multiplier = value
+          strum_selection()
+        end 
+      }      
+    }
+  end
+  
+  --create/show the dialog window
+  if not window_obj or not window_obj.visible then
+    window_obj = app:show_custom_dialog(window_title, window_content)
+  else window_obj:show()
+  end  
+  
+  return(true)
+end
+
+--SELECTION-BASED STRUM-----------------------------------------------
+local function selection_based_strum()
+  local result = clear_variables()
+  if result then result = find_notes_in_selection() end
+  if result then result = calculate_range() end
+  if result then result = calculate_note_placements() end
+  if result then result = show_window() end
+
+end
+
+--MENU/HOTKEY ENTRIES-------------------------------------------------------------------------------- 
+
+renoise.tool():add_menu_entry { 
+  name = "Main Menu:Tools:Strum Selection...", 
+  invoke = function() selection_based_strum() end 
+}
